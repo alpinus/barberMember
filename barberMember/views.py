6898ef30814422842624922503290445ed6cb0e7
dongcai <dongcai.zhao@gmail.com>
2019-06-01 17:38:53 +0800

    view can work now

--- barberMember/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-def member(request,member_id) :
-	response = "hello, your memberid is %s." 
-	return HttpResponse(response % member_id)
-# Create your views here.
--- barber_app/views.py
+++ barber_app/views.py
@@ -1,5 +1,14 @@
+
+
 from django.shortcuts import render
+from django.http import HttpResponse
+#from .barberMember.models import Member
+
+def index(request):
+	return HttpResponse("nothing gose here...")
+
 def member(request,member_id) :
-	response = "hello, your memberid is %s." 
-	return HttpResponse(response % member_id)
+	#response = "hello, your memberid is %s." 
+	#return HttpResponse(response % member_id)
+	return HttpResponse('sssssssssss')
 # Create your views here.
